bbbcba56ed6790af62b810557ecbeb71c6d6eb65
dev-abhisheksh <abhisheksharmaazpm2@gmail.com>
2026-06-25 17:53:35 +0530

    Update API base URL to use environment variable and add Vercel configuration for rewrites

--- src/api/axiosInstance.api.js
+++ src/api/axiosInstance.api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const API = axios.create({
-    baseURL: "http://localhost:4000/api/v1",
+    baseURL: import.meta.env.VITE_API_URL || "http://localhost:4000/api/v1",
     withCredentials: true
 })
