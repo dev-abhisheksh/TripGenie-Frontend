2401d2d9cd9482bd8f7b6322662146c69b1cbc3f
dev-abhisheksh <abhisheksharmaazpm2@gmail.com>
2026-06-25 17:59:01 +0530

    Update API base URL to point to production environment

--- src/api/axiosInstance.api.js
+++ src/api/axiosInstance.api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const API = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || "http://localhost:4000/api/v1",
+    baseURL: "https://tripgenie-9yhp.onrender.com/api/v1",
     withCredentials: true
 })
